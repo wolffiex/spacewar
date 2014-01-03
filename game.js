21a93117acf57b2937a17df5aef00ed07d62eea2
Adam Wolff <wolffiex@fb.com>
2014-01-03 13:34:07 -0800

    Don't make so many arrays

--- game.js
+++ game.js
@@ -17,22 +17,20 @@ function initGame(canvas){
     'right': 39,
   }
 
-  var allKeyUps = Rx.Observable.fromEvent(document, 'keyup', function(args) {
-    return [args[0].keyCode, false];
-  });
+  // Get keyCode of first event
+  var getKeyCode = function(args) {
+    return args[0].keyCode;
+  }
 
-  var allKeyDowns = Rx.Observable.fromEvent(document, 'keydown', function(args) {
-    return [args[0].keyCode, true];
-  });
+  var allKeyUps = Rx.Observable.fromEvent(document, 'keyup', getKeyCode);
+  var allKeyDowns = Rx.Observable.fromEvent(document, 'keydown', getKeyCode);
 
   function keyStream(keyCode) {
-    function isKey(keyPair) {
-      return keyPair[0] == keyCode;
-    }
-
-    return allKeyUps.filter(isKey)
-      .merge(allKeyDowns.filter(isKey))
-      .distinctUntilChanged();
+    var isKey = eq(keyCode);
+    return allKeyUps.filter(isKey).map(constF(false))
+      .merge(
+        allKeyDowns.filter(isKey).map(constF(true))
+      ).distinctUntilChanged();
   };
 
 
@@ -44,7 +42,7 @@ function initGame(canvas){
 
   function valueForDownKey(keyStream, val) {
     return keyStream.combineLatest(timer, function(l, r) {
-      return l[1] ? val : 0;
+      return l ? val : 0;
     }).filter(isNonZero);
   };
 
@@ -68,3 +66,12 @@ function mapDict(obj, f) {
 }
 
 var isNonZero = function(v) {return v != 0;};
+
+var eq = function(val) {
+  return function(input) {
+    return input == val;
+  }
+}
+var constF = function(val) {
+  return function() {return val};
+}
