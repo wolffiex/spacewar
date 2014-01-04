bd7ae91f2cc3653f0b3bed985959518ec5a9410c
Adam Wolff <wolffiex@fb.com>
2014-01-04 02:00:46 -0800

    Thrust!

--- game.js
+++ game.js
@@ -1,23 +1,35 @@
 var shipPoly = [
-  [ 15, 0],
-  [-15, 10],
-  [-7, 0],
-  [-15,-10],
+  [  0, -15],
+  [-10,  15],
+  [  0,   4],
+  [ 10,  15],
 ];
 
-function drawShip(ctx, dx, dy, r) {
+function rotatePoint(pt, r) {
+  var x = pt[0];
+  var y = pt[1];
+
+  var x2 = x * Math.cos(r) - y * Math.sin(r);
+  var y2 = x * Math.sin(r) + y * Math.cos(r);
+  return [x2, y2];
+}
+
+function translatePt(pt, dxdy) {
+  return [pt[0] + dxdy[0], pt[1] + dxdy[1]];
+}
+
+function drawShip(ctx, pos, r) {
   var ship = shipPoly.map(function(pt) {
-    var x = pt[0];
-    var y = pt[1];
+    var newPt = rotatePoint(pt, r);
+    // don't make an extra array
+    newPt[0] += pos[0];
+    newPt[1] += pos[1];
 
-    x2 = x * Math.cos(r) - y * Math.sin(r);
-    y2 = x * Math.sin(r) + y * Math.cos(r);
-
-    return [x2 + dx, y2 + dy];
+    return newPt;
   });
 
   // clear
-  ctx.clearRect(dx - 20, dy - 20, 40, 40);
+  ctx.clearRect(pos[0] - 20, pos[1] - 20, 40, 40);
 
   ctx.fillStyle = '#F00';
   ctx.beginPath();
@@ -39,9 +51,9 @@ function initGame(canvas){
   var timer = Rx.Observable.timer(10, 10);
 
   var keyMap = {
-    'left' : 37,
-    'up'   : 38,
-    'right': 39,
+    'left'   : 37,
+    'thrust' : 38,
+    'right'  : 39,
   }
 
   // Get keyCode of first event
@@ -54,43 +66,53 @@ function initGame(canvas){
 
   function keyStream(keyCode) {
     var isKey = eq(keyCode);
-    return allKeyUps.filter(isKey).map(constF(false))
-      .merge(
-        allKeyDowns.filter(isKey).map(constF(true))
-      ).distinctUntilChanged();
+    return Rx.Observable.returnValue(false).concat(
+      allKeyUps.filter(isKey).map(constF(false))
+        .merge(
+          allKeyDowns.filter(isKey).map(constF(true))
+        ).distinctUntilChanged());
   };
 
 
   var leftKey = keyStream(keyMap.left);
   var rightKey = keyStream(keyMap.right);
 
-  var rotSpeed = 0.01;
-  var leftRot = valueForDownKey(leftKey, rotSpeed * -1);
-  var rightRot = valueForDownKey(rightKey, rotSpeed );
+  var rotSpeed = 0.03;
 
-  function valueForDownKey(keyStream, val) {
-    return keyStream.combineLatest(timer, function(isDown, t) {
-      return isDown ? val : 0;
-    }).filter(isNonZero);
-  };
+  var ir = 0;
+  var rotation = timer.combineLatest(leftKey, rightKey, 
+    function(t, leftIsDown, rightIsDown) {
+      var v = 0;
+      if (leftIsDown) v -= rotSpeed;
+      if (rightIsDown) v += rotSpeed;
+      return v;
+    }).scan(ir, function(acc, v) {
+      return acc + v;
+    });
 
-  var rotation = leftRot.merge(rightRot).scan(0, function(acc, v) {
-    return acc + v;
+  var noAccel = [0,0];
+  var thrustSpeed = [0, -0.02];
+
+  var speed = rotation.combineLatest(keyStream(keyMap.thrust),
+    function(r, thrustIsDown) {
+      return thrustIsDown ? rotatePoint(thrustSpeed, r) : noAccel;
+    }).scan([0,0], function(oldSpeed, accel) {
+      return translatePt( oldSpeed, accel);
+    });
+
+  var iPos = [100, 100];
+  var position = speed.scan(iPos, function(oldPos, speed) {
+    return translatePt(oldPos, speed);
   });
 
-  rotation.subscribe(function(val) {
-    drawShip(ctx, 100, 100, val);
+  var shipCoords = rotation.zip(position, function(r, pt) {
+    return [pt, r];
   });
 
-}
+  shipCoords.subscribe(function(v) {
+    drawShip(ctx, v[0], v[1]);
+  });
 
-function mapDict(obj, f) {
-  var newDict = {};
-  for (var k in obj) {
-    newDict[k] = f(k);
-  }
-
-  return newDict;
 }
 
 var isNonZero = function(v) {return v != 0;};
