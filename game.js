636f0caefb21ade479e90b6a255e58184350e7f2
Adam Wolff <wolffiex@fb.com>
2014-01-03 16:19:39 -0800

    Ship rotates

--- game.js
+++ game.js
@@ -1,15 +1,42 @@
+var shipPoly = [
+  [ 15, 0],
+  [-15, 10],
+  [-7, 0],
+  [-15,-10],
+];
+
+function drawShip(ctx, dx, dy, r) {
+  var ship = shipPoly.map(function(pt) {
+    var x = pt[0];
+    var y = pt[1];
+
+    x2 = x * Math.cos(r) - y * Math.sin(r);
+    y2 = x * Math.sin(r) + y * Math.cos(r);
+
+    return [x2 + dx, y2 + dy];
+  });
+
+  // clear
+  ctx.clearRect(dx - 20, dy - 20, 40, 40);
+
+  ctx.fillStyle = '#F00';
+  ctx.beginPath();
+
+  var start = _.last(ship); 
+  ctx.moveTo(start[0], start[1]);
+
+  _.forEach(ship, function(pt) {
+    ctx.lineTo(pt[0], pt[1]);
+  });
+
+  ctx.fill();
+}
+
 function initGame(canvas){
   var ctx = canvas.getContext('2d');
-  // Create gradient
-  var grd=ctx.createLinearGradient(0,0,200,0);
-  grd.addColorStop(0,"red");
-  grd.addColorStop(1,"white");
+  drawShip(ctx, 100, 100, 0);
 
-  // Fill with gradient
-  ctx.fillStyle=grd;
-  ctx.fillRect(10,10,150,80);
-
-  var timer = Rx.Observable.timer(100, 100);
+  var timer = Rx.Observable.timer(10, 10);
 
   var keyMap = {
     'left' : 37,
@@ -36,13 +63,14 @@ function initGame(canvas){
 
   var leftKey = keyStream(keyMap.left);
   var rightKey = keyStream(keyMap.right);
-  var rotSpeed = .2;
+
+  var rotSpeed = 0.01;
   var leftRot = valueForDownKey(leftKey, rotSpeed * -1);
   var rightRot = valueForDownKey(rightKey, rotSpeed );
 
   function valueForDownKey(keyStream, val) {
-    return keyStream.combineLatest(timer, function(l, r) {
-      return l ? val : 0;
+    return keyStream.combineLatest(timer, function(isDown, t) {
+      return isDown ? val : 0;
     }).filter(isNonZero);
   };
 
@@ -51,7 +79,7 @@ function initGame(canvas){
   });
 
   rotation.subscribe(function(val) {
-    console.log(val)
+    drawShip(ctx, 100, 100, val);
   });
 
 }
